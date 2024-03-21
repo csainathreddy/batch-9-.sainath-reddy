0b57b6ab3dcef40aecff8fa06a1c3f70793b3ab7
csainathreddy <162982760+csainathreddy@users.noreply.github.com>
2024-03-21 11:40:45 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,43 @@
+#print(b)
+#print(c)
+#a,b,c=9
+a=7
+b=5
+#eg;9
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+a=a*b=35
+b=a/b=35/7=5.0
+a=a/b
+print(a,b)
+import keyword
+print(keyword.kwlist)
+#To check if the string is key word or not
+print(key word.is keyword
+11 = list()
+
+a=78
+A=78
+# A variable is a consider to be constant when it defines
+#in caps
+#a=78 # a is variable
+#A=78 # A is constant
+11 =[6, 7,, 8, 0)
+n1=89+7j
+ print(2**4)#-->16
+ #!Assignment-- +-=,-=,
+print(not(a>8anda<10)
+#eg:if
+a=6 if
+print(yes)
+# a number is even or add
+n=int(input(" enter the number:"))
+#name,age ,nationality
+#18 above, indian
+name=input("enter the name:")
+age=int(input ("enter the age:"))
+if age>=18and nationality =="indian":
+      pr
+  
